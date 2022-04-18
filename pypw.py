5b43cd8791c959756e2b9a98a49a8f2207c45a81
spv00 <voseapas@web.de>
2022-04-18 16:34:28 +0200

    and now it can actually generate string passwords

--- pypw.py
+++ pypw.py
@@ -2,21 +2,31 @@
 
 # Secure password generator written in python.
 
-import threading
+import os
 import sys
 import util
 
-platform = sys.platform
+args = sys.argv
+length = 26
+try:
+    length = int(args[1])
+except Exception:
+    pass
 
-length = 32
+platform = sys.platform
 
 out = ""
 
 # Generate random numbers from
-# Do NOT TOUCH THIS CODE! It works, and as long as it works you shouldnt even get close to it.
+# This perticular piece of code works and as long as it works you shouldnt even get close to it.
 mouse = [pos for pos in util.captureRandomMouse(1000)]
+# This is a list comprehension. Also known as the cancer of python. But it looks cool so i use it :D
 rand = ''.join([(str(i[0]+i[1])[-1:]) for i in mouse])
 print()
 
 seed = util.hashPasses(rand, 1000000, 0)
-print(util.createPass(seed, 5))
+password = util.createPass(seed, length)
+
+print("-" * os.get_terminal_size().columns)
+print(password)
+print("-" * os.get_terminal_size().columns)
